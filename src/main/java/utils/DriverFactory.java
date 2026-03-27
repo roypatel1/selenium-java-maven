e424167280499b3d2d7c01d326c5a8db1a501410
Rutul Patel <rp@MacBook-Pro.local>
2026-03-26 22:40:46 -0700

    fix ci

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -23,39 +23,25 @@ public class DriverFactory {
             case "chrome":
                 ChromeOptions options = new ChromeOptions();
                 String env = ConfigReader.getEnv();
-                if (env != null && env.equals("ci")) {
+                if ("ci".equalsIgnoreCase(env)) {
                     options.addArguments("--no-sandbox");
                     options.addArguments("--disable-dev-shm-usage");
-                } else if (env != null && env.equals("local")) {
-                    //create directory in your local by executing this command mkdir -p /Users/rp/selenium-profile
-                    //options.addArguments("user-data-dir=/Users/rp/selenium-profile");
-                    options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+                    options.addArguments("--window-size=1920,1080");
                 }
-                if(ConfigReader.getHeadless()){
+                if (ConfigReader.getHeadless()) {
                     options.addArguments("--headless=new");
                 }
-                // Removes the "Chrome is being controlled by automated software" notification
-                options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-                options.setExperimentalOption("useAutomationExtension", false);
-                // Hides the automation flag from the browser's internal engine
-                options.addArguments("--disable-blink-features=AutomationControlled");
-                // Replace this with the User Agent you copied from chrome://version/
-                options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36");
-                options.addArguments("--disable-blink-features=AutomationControlled");
-                options.addArguments("--start-maximized");
                 options.addArguments("--disable-notifications");
                 options.addArguments("--disable-geolocation");
-                options.addArguments("--remote-allow-origins=*");
                 options.addArguments("--lang=en");
-                options.addArguments("--disable-infobars");
-
+                options.addArguments("--disable-blink-features=AutomationControlled");
                 WebDriverManager.chromedriver().setup();
-
                 driver.set(new ChromeDriver(options));
                 driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-                driver.get().manage().window().maximize();
+                if (!ConfigReader.getHeadless()) {
+                    driver.get().manage().window().maximize();
+                }
                 return driver.get();
-
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions ffOptions = new FirefoxOptions();
@@ -96,4 +82,39 @@ public class DriverFactory {
         driver.get().quit();
         driver.remove();
     }
+
+    /* ChromeOptions options = new ChromeOptions();
+                String env = ConfigReader.getEnv();
+                if (env != null && env.equals("ci")) {
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                } else if (env != null && env.equals("local")) {
+                    //create directory in your local by executing this command mkdir -p /Users/rp/selenium-profile
+                    //options.addArguments("user-data-dir=/Users/rp/selenium-profile");
+                    options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+                }
+                if(ConfigReader.getHeadless()){
+                    options.addArguments("--headless=new");
+                }
+                // Removes the "Chrome is being controlled by automated software" notification
+                options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+                options.setExperimentalOption("useAutomationExtension", false);
+                // Hides the automation flag from the browser's internal engine
+                options.addArguments("--disable-blink-features=AutomationControlled");
+                // Replace this with the User Agent you copied from chrome://version/
+                options.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36");
+                options.addArguments("--disable-blink-features=AutomationControlled");
+                options.addArguments("--start-maximized");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--disable-geolocation");
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--lang=en");
+                options.addArguments("--disable-infobars");
+
+                WebDriverManager.chromedriver().setup();
+
+                driver.set(new ChromeDriver(options));
+                driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+                driver.get().manage().window().maximize();
+                return driver.get();*/
 }
